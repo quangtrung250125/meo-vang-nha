2a5922ccd16effa96d64bce8c6c1b5a7e72b8943
huhuhaha08 <builinhcl2005@gmail.com>
2026-09-19 15:14:09 +0700

    feat(mai-linh): tich hop 5-Star Services, Loyalty VIP card, TopBar va Footer theo mau yeu cau

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,32 @@ import Checkout from './pages/Checkout';
 import { PetProvider } from './contexts/PetContext';
 import { CustomerProvider } from './contexts/CustomerContext';
 import { BookingHistoryProvider } from './contexts/BookingHistoryContext';
-
+import { UIProvider } from './contexts/UIContext';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <CustomerProvider>
-      <PetProvider>
-        <BookingHistoryProvider>
-          <Toaster position="top-center" reverseOrder={false} />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<MainLayout />}>
-                <Route index element={<Home />} />
-                <Route path="services" element={<Services />} />
-                <Route path="booking" element={<Booking />} />
-                <Route path="pet-profile" element={<PetProfile />} />
-                <Route path="my-booking" element={<MyBooking />} />
-                <Route path="tracking" element={<Tracking />} />
-                <Route path="checkout" element={<Checkout />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </BookingHistoryProvider>
-      </PetProvider>
+      <UIProvider>
+        <PetProvider>
+          <BookingHistoryProvider>
+            <Toaster position="top-center" reverseOrder={false} />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<MainLayout />}>
+                  <Route index element={<Home />} />
+                  <Route path="services" element={<Services />} />
+                  <Route path="booking" element={<Booking />} />
+                  <Route path="pet-profile" element={<PetProfile />} />
+                  <Route path="my-booking" element={<MyBooking />} />
+                  <Route path="tracking" element={<Tracking />} />
+                  <Route path="checkout" element={<Checkout />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </BookingHistoryProvider>
+        </PetProvider>
+      </UIProvider>
     </CustomerProvider>
   );
 }
